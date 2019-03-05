489de0d9a5e1808a397dac8d47c6f01adf091f7b
surpassm <lustres@sina.com>
2019-03-05 18:17:26 +0800

    新增demo基础权限结构mybatis

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @ComponentScan({"com.example.demo","com.github.surpassm.security","com.github.surpassm.config",})
+@MapperScan(basePackages = "com.example.demo.mapper")
 @SpringBootApplication
 @EnableSwagger2Doc
 public class DemoApplication {

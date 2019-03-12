64563f4e480450b268662e9181ed4969f4fce0ac
surpassm <lustres@sina.com>
2019-03-12 17:02:37 +0800

    修正请求头获取

--- config-spring-boot-starter/src/main/java/com/github/surpassm/config/TokenInterceptor.java
+++ config-spring-boot-starter/src/main/java/com/github/surpassm/config/TokenInterceptor.java
@@ -27,7 +27,7 @@ public class TokenInterceptor extends HandlerInterceptorAdapter {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		String header = request.getHeader("Authorization");
 		if (header != null && header.startsWith("Bearer ")) {
-			String token = StringUtils.strip(header,"Bearer ");
+			String token = header.substring(7);
 			request.setAttribute("Authorization", token);
 			return true;
 		}
